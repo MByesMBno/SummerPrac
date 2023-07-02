d0729be8605ccc9b5530878ad1047911065bdf80
MByesMBno <akamegakill228@mail.ru>
2023-07-03 02:56:52 +0500

    "2:56 03/07/23 Paul"

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faHome, faRotateRight, faFire, faFaceSmile, faChild, faWandSparkles, faUser, faArrowRightFromBracket} from '@fortawesome/free-solid-svg-icons'
+import {faHome, faRotateRight, faFire, faFaceSmile, faChild, faWandSparkles, faUser, faArrowRightFromBracket, faComment} from '@fortawesome/free-solid-svg-icons'
 import './Header.css'
 import { NavLink } from 'react-router-dom'
 
